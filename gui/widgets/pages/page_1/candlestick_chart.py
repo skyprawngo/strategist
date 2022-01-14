994a59ffb3928c29ecfffb9ece10576148fe316e
skyprawngo <skyprawngo@gmail.com>
2022-01-15 05:52:19 +0900

    Update candlestick_chart.py

--- gui/widgets/pages/page_1/candlestick_chart.py
+++ gui/widgets/pages/page_1/candlestick_chart.py
@@ -29,6 +29,7 @@ class Candlestick_Chart(QWidget):
         self.vol_series = QCandlestickSeries()
         self.vol_series.setIncreasingColor(Qt.red)
         self.vol_series.setDecreasingColor(Qt.blue)
+        interest = 0
         for row_num in range(len(df)):
             
             candlestick = QCandlestickSet(df.iloc[row_num][1], df.iloc[row_num][2], df.iloc[row_num][3], df.iloc[row_num][4], float(df.iloc[row_num][0]))
@@ -39,12 +40,18 @@ class Candlestick_Chart(QWidget):
             else:
                 vol_candlestick = QCandlestickSet(df.iloc[row_num][5], df.iloc[row_num][5], 0, 0, float(df.iloc[row_num][0]))
             self.vol_series.append(vol_candlestick)
-                
+            
+            vbscheck = df.iloc[row_num][1] + (df.iloc[row_num-1][2]-df.iloc[row_num-1][3])*0.5
+            if df.iloc[row_num][4] > vbscheck :
+                interest += df.iloc[row_num][4]-vbscheck
+        
+        print(interest)
             
         self.chart = QChart()
+        self.chart.setTitle("BTC/USDT")
         self.chart.legend().hide() 
         self.chart.addSeries(self._series)
-        
+
         self.vol_chart = QChart()
         self.vol_chart.legend().hide() 
         self.vol_chart.addSeries(self.vol_series)
@@ -53,20 +60,31 @@ class Candlestick_Chart(QWidget):
         axis_x = QDateTimeAxis()
         axis_x.setFormat("yy/MM/dd")
         axis_x.setLabelsVisible(True)
+        axis_x.setTitleVisible(False)
         self.chart.addAxis(axis_x, Qt.AlignBottom)
         self._series.attachAxis(axis_x)
+        
+        axis_y = QValueAxis()
+        self.chart.addAxis(axis_y, Qt.AlignLeft)
+        self._series.attachAxis(axis_y)
         
         vol_axis_x = QDateTimeAxis()
         vol_axis_x.setFormat("yy/MM/dd")
         vol_axis_x.setLabelsVisible(False)
+        vol_axis_x.setTitleVisible(False)
         self.vol_chart.addAxis(vol_axis_x, Qt.AlignBottom)
         self.vol_series.attachAxis(vol_axis_x)
         
+        vol_axis_y = QValueAxis()
+        self.vol_chart.addAxis(vol_axis_y, Qt.AlignLeft)
+        self.vol_series.attachAxis(vol_axis_y)
+        
         self.chartview = QChartView(self.chart)
+        
         self._layout.addWidget(self.chartview)
         
         self.vol_chartview = QChartView(self.vol_chart)
-        self.vol_chartview.setFixedHeight(100)
+        self.vol_chartview.setFixedHeight(130)
         self._layout.addWidget(self.vol_chartview)
     
     def sig_received(self, tooltip_text):
